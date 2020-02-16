923d22f5b7525fcc2f6eca8ff693ba8a63640c4a
James McCarthy <jamesmccarthyxci@gmail.com>
2020-02-16 15:04:32 -0500

    Environment var config

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,4 @@
 module.exports = {
-  
+  contentful_space_id: process.env.GATSBY_CONTENTFUL_SPACE_ID,
+  contentful_access_token: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN,
 }
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,4 @@
-const config = require("./config/dev")
+const config = require("./config/keys")
 
 module.exports = {
   siteMetadata: {
